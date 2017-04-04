23412595eaa16076fbc274a81b2d69ae2a63b973
shiverenje <fshivec@gmail.com>
2017-04-04 19:55:49 +0300

    Add files via upload

---
+++ Day 2/fizzbuzz.py
@@ -0,0 +1,12 @@
+def fizz_buzz(n):
+    if n > 0:
+        if n % 5 == 0 and n % 3 != 0:
+            return 'buzz'
+        elif n % 3 == 0 and n % 5 != 0:
+            return 'fizz'
+        elif n % 5 == 0 and n % 3 == 0:
+            return ('fizzBuzz')
+        else:
+            return n
+    else:
+        return 'Invalid argument'
